22c15e2291546128066573ab0bda04b80d996859
durfsurn <durfsurn@gmail.com>
2025-06-11 21:41:05 +1000

    docs

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-wasm-pack build --target web --no-typescript --no-pack --release --out-dir ./wasm
-rm ./wasm/.gitignore
+wasm-pack build --target web --no-typescript --no-pack --release --out-dir ./docs
+rm ./docs/.gitignore
